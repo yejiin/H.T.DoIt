33a1c41fe46bc4925db2cd5889c288f88119cee9
yjsong67 <yj_song67@naver.com>
2020-11-13 21:40:22 +0900

    [Add] login, signup

--- routes/home.js
+++ routes/home.js
@@ -1,8 +1,23 @@
 var express = require('express');
 var router = express.Router();
+var db_config = require('../config/database.js');
+var conn = db_config.init();
 
+db_config.connect(conn);
+
+// Home
 router.get('/', function(req, res){
     res.render('home');
+});
+
+// Login
+router.get('/login', function(req, res){
+    res.render('form/login');
+});
+
+// Signup
+router.get('/signup', function(req, res){
+    res.render('form/signup');
 });
 
 module.exports = router;
